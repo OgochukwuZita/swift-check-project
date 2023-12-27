ca3f01fda60e3473888d57f8b84e9d71af6e1f23
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2023-12-27 16:04:14 +0100

    concluded media queries

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
-'use strict';
+// 'use strict';
 
-const Todo = localStorage.getItem('todoz') ? JSON.parse(localStorage.getItem('todoz')) : [];
+// const Todo = localStorage.getItem('todoz') ? JSON.parse(localStorage.getItem('todoz')) : [];
 
-document.addEventListener('DOMContentLoaded', function () {
-    const dailyQuoteElement = document.getElementById('dailyQuote');
-    const toDo = document.getElementById('todos-container'); // Define toDo here
+// document.addEventListener('DOMContentLoaded', function () {
+//     const dailyQuoteElement = document.getElementById('dailyQuote');
+//     const toDo = document.getElementById('todos-container'); // Define toDo here
 
-    function fadeOut() {
-        dailyQuoteElement.classList.add('daily-quote');
-        setTimeout(updateQuote, 60000);
-    }
+//     function fadeOut() {
+//         dailyQuoteElement.classList.add('daily-quote');
+//         setTimeout(updateQuote, 60000);
+//     }
 
-    function fadeIn() {
-        dailyQuoteElement.classList.remove('daily-quote');
-    }
+//     function fadeIn() {
+//         dailyQuoteElement.classList.remove('daily-quote');
+//     }
 
-    function updateQuote() {
-        const dailyQuotes = [
-            // ... (your daily quotes array)
-        ];
+//     function updateQuote() {
+//         const dailyQuotes = [
+//             // ... (your daily quotes array)
+//         ];
 
-        const randomIndex = Math.floor(Math.random() * dailyQuotes.length);
-        const randomQuote = dailyQuotes[randomIndex];
+//         const randomIndex = Math.floor(Math.random() * dailyQuotes.length);
+//         const randomQuote = dailyQuotes[randomIndex];
 
-        fadeOut();
+//         fadeOut();
 
-        setTimeout(function () {
-            dailyQuoteElement.textContent = randomQuote;
-            fadeIn();
-        }, 60000);
-    }
+//         setTimeout(function () {
+//             dailyQuoteElement.textContent = randomQuote;
+//             fadeIn();
+//         }, 60000);
+//     }
 
-    updateQuote();
+//     updateQuote();
 
-    const addBtn = document.getElementById('addBtn');
-    const textField = document.getElementById('text-input-field');
+//     const addBtn = document.getElementById('addBtn');
+//     const textField = document.getElementById('text-input-field');
 
-    function createTask() {
-        addBtn.addEventListener('click', function () {
-            if (textField.value.trim() === '') {
-                alert('Please enter a task!');
-            } else {
-                Todo.unshift(textField.value);
-                localStorage.setItem('todoz', JSON.stringify(Todo));
-                textField.value = "";
-                renderTodo();
-            }
-        });
-    }
+//     function createTask() {
+//         addBtn.addEventListener('click', function () {
+//             if (textField.value.trim() === '') {
+//                 alert('Please enter a task!');
+//             } else {
+//                 Todo.unshift(textField.value);
+//                 localStorage.setItem('todoz', JSON.stringify(Todo));
+//                 textField.value = "";
+//                 renderTodo();
+//             }
+//         });
+//     }
 
-    createTask();
-    renderTodo(); // Initial rendering
+//     createTask();
 
-});
+// });
